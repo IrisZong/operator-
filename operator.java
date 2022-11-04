36de4bf685b82178a3ebbec0845f563a7f456d50
IrisZong <77033184+IrisZong@users.noreply.github.com>
2022-11-04 14:03:04 -0400

    Add files via upload

---
+++ operator.java
@@ -0,0 +1,33 @@
+
+
+
+public class operator {
+
+    public static void main(String args[]) {  
+
+     int a=5, b=8;
+     boolean c;
+
+    /*  System.out.println(a++); // postincreasment a++ inc after excuting
+     
+     System.out.println(a);
+
+     System.out.println(++a);  //pre increasment, before excution
+    */
+
+     //c= ++a + b-a;
+    
+     //System.out.println("a now is "+ a);
+
+     //System.out.println(c);
+
+
+    // System.out.println(" a++ + b-a is "+(a++ + b-a));
+    // System.out.println(" ++a + b-a is "+(++a + b-a));
+
+     c= (a >= (b*a));
+     System.out.println(c);
+
+ 
+}
+}
